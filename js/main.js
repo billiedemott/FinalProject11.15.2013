079907cfc061773a7edef172ca5bd4e213a46fa2
Billie DeMott <billiemdemott@gmail.com>
2013-11-20 20:34:08 -0500

    Latest Version - some styling on the divs
    need styling; need to fix bugs in uxdesign

--- js/main.js
+++ js/main.js
@@ -87,49 +87,6 @@ function init() {
 		// Arc and rotate back to the beginning.
 		.arc(1300, 50, 900, -Math.PI/2, -Math.PI, true, {rotate: Math.PI*2, name: "end"});
 
-
-
-		// .moveTo(-1250,700)
-		// .lineTo (-1000,2900)
-		// .arc(-1300, 2700, 600, 0, -Math.PI/2, true
-		// .lineTo (-300,1700)
-		// want arc here
-
-		// Arc and rotate back to the beginning.
-		//.arc(1300, 50, 900, -Math.PI/2, -Math.PI, true, {rotate: Math.PI*2, name: "end"});
-
-
-
-
-
-
-	
-
-
-		// Arc up while rotating
-		// .arc(1800, 1000, 600, Math.PI/2, 0, true, {rotate: Math.PI/2 })
-		// // Line to 'rotations'
-		// .lineTo(2400, 750, {
-		// 	name: "rotations"
-		// })
-		// // Rotate in place
-		// .rotate(3*Math.PI/2, {
-		// 	name: "rotations-rotated"
-		// })
-		// Continue upwards to 'source'
-		// .lineTo(2400, -700, {
-		// 	name: "source"
-		// })
-		// Small arc downwards
-		// .arc(2250, -700, 150, 0, -Math.PI/2, true)
-
-		//Line to 'follow'
-		// .lineTo(1350, -850, {
-		// 	name: "follow"
-		// })
-		// Arc and rotate back to the beginning.
-		//.arc(1300, 50, 900, -Math.PI/2, -Math.PI, true, {rotate: Math.PI*2, name: "end"});
-
 	 // ========== CALLING THE SCROLLPATH PLUGIN==============*//
 	$(".wrapper").scrollPath({drawPath: true, wrapAround: true});
 
@@ -155,7 +112,7 @@ function init() {
 //DELETE
 	/* ===================================================================== */
 
-
+/*
 	$(".settings .show-path").click(function(e) {
 		e.preventDefault();
 		$(".sp-canvas").toggle();
@@ -165,6 +122,7 @@ function init() {
 		$(this).text("Show Path");
 	});
 
+*/
 	$(".tweet").click(function(e) {
 		open(this.href, "", "width=550, height=450");
 		e.preventDefault();
@@ -196,29 +154,6 @@ function ordinal(num) {
 }
 */
 /* ========== DEFINING THE FORM VALIDATION ============= */
-/*
-$('form').submit (
-	function() {
-		console.log('clicked');
-		var name = $('first-name-input').val();
-
-		var isChecked = $('input: checked').length;
-
-		var isValid = 1; 
-
-		if (name != "" && email != "") alert ('validated')
-
-		if (name == " ") isValid = 0; 
-	    else if (email == "") isValid =0; 
-
-
-		var email = $('last-name-input').val();
-		console.log($('email-input').val());
-		return false; 
-		// way to prevent the default behavior from happening; 
-		// delays submission
-	});
-*/
 
 function validateForm() {
 	var formBody = $('.contact-form-body');
